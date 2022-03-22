2af010ea48a6f206e8b05c44d3b21311d099d7af
beastlex <beastlex@gmail.com>
2022-03-22 03:52:27 +0000

    2022-03-22: add XML serialize

--- main.go
+++ main.go
@@ -1,16 +1,28 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"encoding/xml"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Person struct {
+	XMLName xml.Name `xml:"person"`
+	FirstName string `xml:"firstName,attr"`
+	LastName string `xml:"lastName,attr"`
+}
 
 func IndexHandler(c *gin.Context) {
 	name := c.Params.ByName("name")
-	c.JSON(200, gin.H{
-		"message": "hello " + name,
+	surname := c.Params.ByName("surname")
+	c.XML(200, Person{
+		FirstName: name,
+		LastName: surname,
 	})
 }
 
 func main() {
 	router := gin.Default()
-	router.GET("/:name", IndexHandler)
+	router.GET("/:name/:surname", IndexHandler)
 	router.Run()
 }
